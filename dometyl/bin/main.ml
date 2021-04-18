dfab1b8a04a8270cbb7874551d0a2f223a02d836
Geoff deRosenroll <geoffderosenroll@gmail.com>
2021-04-17 21:08:29 -0700

    Getting started

--- dometyl/bin/main.ml
+++ dometyl/bin/main.ml
@@ -1,4 +1,5 @@
 let () =
   print_endline "Building scads...";
-  Scad_ml.Util.write (open_out "case.scad") Dometyl.Case.scad;
+  Scad_ml.Util.write (open_out "keyhole.scad") Dometyl.Case.KeyHole.scad;
+  Scad_ml.Util.write (open_out "column.scad") Dometyl.Case.Column.scad;
   print_endline "Done!"
--- dometyl/lib/case.ml
+++ dometyl/lib/case.ml
@@ -1,3 +1,16 @@
 open Scad_ml
 
-let scad = Model.sphere 10.
+module KeyHole = struct
+  let outer_w = 19.
+  let inner_w = 14.
+  let thickness = 4.
+
+  let scad =
+    let outer = Model.cube ~center:true (outer_w, outer_w, thickness) in
+    let inner = Model.cube ~center:true (inner_w, inner_w, thickness +. 0.1) in
+    Model.difference outer [ inner ]
+end
+
+module Column = struct
+  let scad = KeyHole.scad
+end
