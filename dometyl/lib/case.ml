ec9fd5fc5110e242fc25c038ec8ae4f786cf4970
Geoff deRosenroll <geoffderosenroll@gmail.com>
2021-04-25 13:48:47 -0700

    Add thumb to plate (WIP).
    TODO: Make some decisions about organization and config for the plate
    and thumb cluster. Next part coming up involving the calculation of
    walls is going to be much harder. So want things to make sense before then.

--- dometyl/lib/case.ml
+++ dometyl/lib/case.ml
@@ -38,46 +38,58 @@ module Plate = struct
   type t =
     { scad : Model.t
     ; columns : Col.t Map.M(Int).t
+    ; thumb : Thumb.t
     }
 
   let n_cols = 5
   let spacing = 1.
   let centre_col = 2
   let tent = Math.pi /. 12.
+  let thumb_offset = -10., -45., 5.
+  let thumb_angle = Math.(pi /. 6., pi /. 5., pi /. -3.)
+
+  (* TODO: would like to actually calculate this. Will need to take offsets,
+   *  total plate depth, tenting, and required guts clearance into account. *)
+  let rise = 18.
 
   (* TODO: tune, these are placeholders *)
-  let offsets =
+  let col_offsets =
     let space = Col.Key.outer_w +. spacing in
     let lookup = function
-      | 2 -> 0., 2.82, -4.5
-      | 3 -> 0., 1.5, -2.
-      | i when i >= 4 -> 0., -12., 5.64
+      | 2 -> 0., 6., -6. (* middle *)
+      | 3 -> 0., 3., -2. (* ring *)
+      | i when i >= 4 -> 0., -12., 6. (* pinky *)
       | _ -> 0., 0., 0.
     in
     let f m i =
-      let data = Util.(lookup i <+> (space *. Float.of_int i, 0., 0.)) in
+      let data = Util.(lookup i <+> (space *. Float.of_int i, 0., rise)) in
       Map.add_exn ~key:i ~data m
     in
     List.fold ~f ~init:(Map.empty (module Int)) (List.range 0 n_cols)
 
-  let centre_offset = Map.find_exn offsets centre_col
+  let centre_offset = Map.find_exn col_offsets centre_col
 
   (* TODO: The x offset subtraction fixed an issue the keyhole crossing the xaxis
    * to be gimped, but I should probably shift the whole model over to avoid
    * any rotation problems all together. (Though this fix is fine to stay in place
    * I think. ) *)
   let place_col off =
-    Col.map
-      ~f:
-        ( Model.translate off
-        >> Util.(
-             rotate_about_pt (0., tent, 0.) (get_x off -. get_x centre_offset, 0., 0.)) )
-      Col.t
+    let f =
+      Model.translate off
+      >> Util.(rotate_about_pt (0., tent, 0.) (get_x off -. get_x centre_offset, 0., 0.))
+    in
+    Col.map ~f Col.t
 
-  let columns = Map.map ~f:place_col offsets
+  let columns = Map.map ~f:place_col col_offsets
+
+  let thumb =
+    Thumb.map
+      ~f:(fun t -> Util.(t |@> thumb_angle |>> (thumb_offset <+> (0., 0., rise))))
+      Thumb.t
 
   let scad =
-    Model.union (Map.fold ~f:(fun ~key:_ ~data l -> data.scad :: l) ~init:[] columns)
+    Model.union
+      (Map.fold ~f:(fun ~key:_ ~data l -> data.scad :: l) ~init:[ thumb.scad ] columns)
 
-  let t = { scad; columns }
+  let t = { scad; columns; thumb }
 end
