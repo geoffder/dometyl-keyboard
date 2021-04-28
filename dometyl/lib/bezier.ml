3e8de61912e86325cd25e7bc182a98dd8dad21d6
Geoff deRosenroll <geoffderosenroll@gmail.com>
2021-04-27 22:29:38 -0700

    Generate curve

--- dometyl/lib/bezier.ml
+++ dometyl/lib/bezier.ml
@@ -16,3 +16,7 @@ let cubic ~p1:(x1, y1) ~p2:(x2, y2) ~p3:(x3, y3) ~p4:(x4, y4) t =
   let x = (a *. x1) +. (b *. x2) +. (c *. x3) +. (d *. x4)
   and y = (a *. y1) +. (b *. y2) +. (c *. y3) +. (d *. y4) in
   x, y
+
+let curve bez dt =
+  let rec loop acc t = if Float.(t <= 1.) then loop (bez t :: acc) (t +. dt) else acc in
+  List.rev (loop [] 0.)
