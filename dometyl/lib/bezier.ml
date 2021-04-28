05c6921a8ebe604fc0754d5e56c4b2f43fc94c0b
Geoff deRosenroll <geoffderosenroll@gmail.com>
2021-04-28 14:59:53 -0700

    Add z dimension

--- dometyl/lib/bezier.ml
+++ dometyl/lib/bezier.ml
@@ -1,21 +1,23 @@
 open! Base
 
-let quad ~p1:(x1, y1) ~p2:(x2, y2) ~p3:(x3, y3) t =
+let quad ~p1:(x1, y1, z1) ~p2:(x2, y2, z2) ~p3:(x3, y3, z3) t =
   let a = (1. -. t) **. 2.
   and b = 2. *. t *. (1. -. t)
   and c = t **. 2. in
   let x = (a *. x1) +. (b *. x2) +. (c *. x3)
-  and y = (a *. y1) +. (b *. y2) +. (c *. y3) in
-  x, y
+  and y = (a *. y1) +. (b *. y2) +. (c *. y3)
+  and z = (a *. z1) +. (b *. z2) +. (c *. z3) in
+  x, y, z
 
-let cubic ~p1:(x1, y1) ~p2:(x2, y2) ~p3:(x3, y3) ~p4:(x4, y4) t =
+let cubic ~p1:(x1, y1, z1) ~p2:(x2, y2, z2) ~p3:(x3, y3, z3) ~p4:(x4, y4, z4) t =
   let a = (1. -. t) **. 3.
   and b = 3. *. t *. ((1. -. t) **. 2.)
   and c = 3. *. (t **. 2.) *. (1. -. t)
   and d = t **. 3. in
   let x = (a *. x1) +. (b *. x2) +. (c *. x3) +. (d *. x4)
-  and y = (a *. y1) +. (b *. y2) +. (c *. y3) +. (d *. y4) in
-  x, y
+  and y = (a *. y1) +. (b *. y2) +. (c *. y3) +. (d *. y4)
+  and z = (a *. z1) +. (b *. z2) +. (c *. z3) +. (d *. z4) in
+  x, y, z
 
 let curve bez dt =
   let rec loop acc t = if Float.(t <= 1.) then loop (bez t :: acc) (t +. dt) else acc in
