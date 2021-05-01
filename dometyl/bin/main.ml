1554f895351aab171cb9fe614bba352ad88fa615
Geoff deRosenroll <geoffderosenroll@gmail.com>
2021-04-30 22:04:27 -0700

    Add output of Niz switch bottom model

--- dometyl/bin/main.ml
+++ dometyl/bin/main.ml
@@ -7,4 +7,5 @@ let () =
   Scad_ml.Util.write (open_out "thumb.scad") Case.Thumb.t.scad;
   Scad_ml.Util.write (open_out "plate.scad") Case.Plate.t.scad;
   Scad_ml.Util.write (open_out "niz_bot.scad") Case.NizBot.t.scad;
+  Scad_ml.Util.write (open_out "niz_bottom.scad") Niz.Bottom.scad;
   print_endline "Done!"
