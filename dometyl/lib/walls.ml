f306d008986f345c5450d01324c97830a29d3ad2
Geoff deRosenroll <geoffderosenroll@gmail.com>
2021-06-24 23:29:43 -0700

    Integrate poly_siding with column_drop

--- dometyl/lib/walls.ml
+++ dometyl/lib/walls.ml
@@ -2,6 +2,9 @@ open Base
 open Scad_ml
 
 module Points = struct
+  (* TODO: there is a lot of replication between this and the KeyHole.Face.Points
+   * module. It's mainly the make which is different. Should amalgamate in own
+   * file, with two different make functions (with adequate names). *)
   type t =
     { top_left : Vec3.t
     ; top_right : Vec3.t
@@ -43,6 +46,21 @@ module Points = struct
   let mark t =
     let f p = Model.cube ~center:true (1., 1., 1.) |> Model.translate p in
     Model.union [ f t.centre; f t.top_right; f t.top_left; f t.bot_right; f t.bot_left ]
+
+  let of_clockwise_list_exn = function
+    | [ top_left; top_right; bot_right; bot_left ] ->
+      { top_left
+      ; top_right
+      ; bot_left
+      ; bot_right
+      ; centre =
+          Vec3.(map (( *. ) 0.25) (top_left <+> top_right <+> bot_left <+> bot_right))
+      }
+    | _ -> failwith "Expect list of length 4, with corner points in clockwise order."
+
+  let of_clockwise_list l =
+    try Ok (of_clockwise_list_exn l) with
+    | Failure e -> Error e
 end
 
 type t =
@@ -61,7 +79,7 @@ let start_chunk side (k : _ KeyHole.t) =
   let centre = Vec3.(face.points.centre <+> t) in
   Model.rotate r cyl |> Model.translate centre, centre
 
-let siding
+let cyl_siding
     ?(x_off = 0.)
     ?(y_off = 0.)
     ?(z_off = 2.)
@@ -115,7 +133,60 @@ let siding
   in
   { scad; points }
 
-let column_drop ?z_off ?d1 ?d2 ?n_steps ~spacing ~columns side idx =
+let poly_siding
+    ?(x_off = 0.)
+    ?(y_off = 0.)
+    ?(z_off = 0.)
+    ?(n_steps = 4)
+    ?(d1 = 4.)
+    ?(d2 = 7.)
+    side
+    (key : _ KeyHole.t)
+  =
+  (* TODO: return a t with points included, rather than a scad. I'll need to change
+   * the functions up a bit to make it not gross. *)
+  let ortho = KeyHole.orthogonal key side in
+  let z_hop = (Float.max 0. (Vec3.get_z ortho) *. key.config.thickness) +. z_off in
+  let face = KeyHole.Faces.face key.faces side in
+  let top_offset =
+    Vec3.(mul (1., 1., 0.) (face.points.bot_right <-> face.points.top_right))
+  in
+  let get_bez top ((x, y, z) as p1) =
+    let jog, d1 =
+      if top then key.config.thickness, d1 +. ((d2 -. d1) /. 2.) else 0., d1
+    in
+    let xy = Vec3.(normalize (mul ortho (1., 1., 0.))) in
+    let p2 =
+      Vec3.(
+        mul xy (d1 +. jog, d1 +. jog, 0.)
+        |> add (x +. x_off, y +. y_off, z +. z_hop)
+        |> add (if top then top_offset else 0., 0., 0.))
+    and p3 =
+      Vec3.(
+        add (mul xy (d2 +. jog, d2 +. jog, 0.)) (x +. x_off, y +. y_off, 0.)
+        |> add (if top then top_offset else 0., 0., 0.))
+    in
+    p3, Bezier.quad_vec3 ~p1 ~p2 ~p3
+  in
+  let cw_points = KeyHole.Face.Points.to_clockwise_list face.points in
+  let steps =
+    let lowest_z =
+      let f m (_, _, z) = Float.min m z in
+      KeyHole.Face.Points.fold ~f ~init:Float.max_value face.points
+    in
+    let adjust (_, _, z) = Float.(to_int (z /. lowest_z *. of_int n_steps)) in
+    `Ragged (List.map ~f:adjust cw_points)
+  and end_ps, bezs =
+    List.foldi
+      ~f:(fun i (ends, bs) p ->
+        let e, b = get_bez (i > 1) p in
+        e :: ends, b :: bs )
+      ~init:([], [])
+      (List.rev cw_points)
+  in
+  { scad = Bezier.prism_exn bezs steps; points = Points.of_clockwise_list_exn end_ps }
+
+let column_drop ?z_off ?d1 ?d2 ?n_steps ~spacing ~columns ?(kind = `Poly) side idx =
   let key, face, hanging =
     let c : _ Column.t = Map.find_exn columns idx in
     match side with
@@ -143,57 +214,31 @@ let column_drop ?z_off ?d1 ?d2 ?n_steps ~spacing ~columns side idx =
       if Float.(diff > 0.) then diff +. spacing else Float.max 0. (spacing +. diff)
     | _           -> 0.
   in
-  siding ~x_off:(x_dodge *. -1.) ?z_off ?d1 ?d2 ?n_steps side key
-
-let poly_siding ?(z_up = 2.) ?(n_steps = 10) side d1 d2 (key : _ KeyHole.t) =
-  (* TODO: return a t with points included, rather than a scad. I'll need to change
-   * the functions up a bit to make it not gross. *)
-  let ortho = KeyHole.orthogonal key side in
-  let z_hop = (Float.max 0. (Vec3.get_z ortho) *. key.config.thickness) +. z_up in
-  let face = KeyHole.Faces.face key.faces side in
-  let top_offset =
-    Vec3.(mul (1., 1., 0.) (face.points.bot_right <-> face.points.top_right))
-  in
-  let lowest_z =
-    let f m (_, _, z) = Float.min m z in
-    KeyHole.Face.Points.fold ~f ~init:Float.max_value face.points
-  in
-  let get_bez top ((x, y, z) as p1) =
-    let jog, d1 =
-      if top then key.config.thickness, d1 +. ((d2 -. d1) /. 2.) else 0., d1
-    in
-    let xy = Vec3.(normalize (mul ortho (1., 1., 0.))) in
-    let p2 =
-      Vec3.(
-        mul xy (d1 +. jog, d1 +. jog, 0.)
-        |> add (x, y, z +. z_hop)
-        |> add (if top then top_offset else 0., 0., 0.))
-    and p3 =
-      Vec3.(
-        add (mul xy (d2 +. jog, d2 +. jog, 0.)) (x, y, 0.)
-        |> add (if top then top_offset else 0., 0., 0.))
-    in
-    Bezier.quad_vec3 ~p1 ~p2 ~p3
-  in
-  let steps =
-    let adjust (_, _, z) = Float.(to_int (z /. lowest_z *. of_int n_steps)) in
-    `Ragged
-      [ adjust face.points.top_right
-      ; adjust face.points.top_left
-      ; adjust face.points.bot_right
-      ; adjust face.points.bot_left
-      ]
-  in
-  Bezier.prism_exn
-    [ get_bez true face.points.top_right
-    ; get_bez true face.points.top_left
-    ; get_bez false face.points.bot_left
-    ; get_bez false face.points.bot_right
-    ]
-    steps
+  match kind with
+  | `Cyl  -> cyl_siding ~x_off:(x_dodge *. -1.) ?z_off ?d1 ?d2 ?n_steps side key
+  | `Poly -> poly_siding ~x_off:(x_dodge *. -1.) ?z_off ?d1 ?d2 ?n_steps side key
 
 let key_bridge k1 k2 = Model.hull KeyHole.[ k1.faces.east.scad; k2.faces.west.scad ]
 let join_bridge j1 j2 = Model.hull Column.Join.[ j1.faces.east; j2.faces.west ]
+
+let poly_bridge (face1 : KeyHole.Face.t) (face2 : KeyHole.Face.t) =
+  (* NOTE: Same results as key_bridge when used like so. A scheme similar to
+   * this could obviously be used for joining other parts, but I'll want to come
+   * up with a way to provide a nice /reliable/ curve between them, instead of
+   * a sharp cut. Alternative is a multipart hull with cylinders. *)
+  let get_bez p1 p3 =
+    let p2 = Vec3.(div (p1 <+> p3) (2., 2., 2.)) in
+    Bezier.quad_vec3 ~p1 ~p2 ~p3
+  in
+  List.map2_exn
+    ~f:get_bez
+    (KeyHole.Face.Points.to_clockwise_list face1.points)
+    [ face2.points.top_right
+    ; face2.points.top_left
+    ; face2.points.bot_left
+    ; face2.points.bot_right
+    ]
+  |> Bezier.prism_exn
 
 let join_columns ~columns a_i b_i =
   let a : _ Column.t = Map.find_exn columns a_i
