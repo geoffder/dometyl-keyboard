a2875a1413b5cdbe790549828b76839c665dda37
Geoff deRosenroll <geoffderosenroll@gmail.com>
2021-07-02 23:52:53 -0700

    Adjust straight base for tight angles

--- dometyl/lib/walls.ml
+++ dometyl/lib/walls.ml
@@ -31,6 +31,15 @@ let prism_exn a b =
  * and southern wall of the middle finger column. Otherwise, the thumbs functions will
  * be separate since the pattern is different there. Makes sense to have the separate
  * modules with their own make functions then. *)
+
+(* TODO:
+ * - cubic bezier base function for use on easter side when there is no siding
+ * there (should be the default, will probably look nicer).
+ * - full wall connection function (try drawing a face for each using the bezier
+ * edges, then pass these to the basic prism generator to try and "hull" them).
+ * - if the polyhedron full connection doesn't work due to overlapping points at
+ * the top, then try making faces by direction sliding subtraction (carried by Wall.t?)
+ * then hull those. *)
 module Body = struct
   type col =
     { north : Wall.t option
@@ -48,9 +57,6 @@ module Body = struct
     }
 
   let bez_base ?(height = 11.) ?(n_steps = 6) (w1 : Wall.t) (w2 : Wall.t) =
-    (* TODO: the directions of the top and bottom face may not be exactly the same
-     * perhaps better to calculate for them separately (forced to use another
-     * parameter for get_bez in that case. ) *)
     let direction Points.{ top_left; top_right; _ } =
       Vec3.normalize Vec3.(top_left <-> top_right)
     in
@@ -87,26 +93,58 @@ module Body = struct
     Bezier.prism_exn bezs steps
 
   let straight_base ?(height = 11.) (w1 : Wall.t) (w2 : Wall.t) =
-    (* TODO: fudge each point into the wall using the direction of the wall
-     * edge that it lies on to ensure reliable union. *)
-    (* let direction Points.{ top_left; top_right; _ } =
-     *   Vec3.normalize Vec3.(top_left <-> top_right)
-     * in
-     * let ((dx, dy, _) as dir1) = direction w1.points
-     * and dir2 = direction w2.points in *)
+    (* TODO: further improve resiliency to steep angles. May not be able to avoid
+     * externally visible (top side) entry of base into wall if I want to make sure
+     * that extreme splay angles or situations like the thumb connection don't break
+     * it. Perhaps, have a flag for if the extreme measure is needed? (then move the
+     * top point on the dest wall so that the wall can be thicker )*)
+    let direction Points.{ top_left; top_right; _ } =
+      Vec3.normalize Vec3.(top_left <-> top_right)
+    in
+    let ((dx, dy, _) as dir1) = direction w1.points
+    and dir2 = direction w2.points in
+    let fudge d =
+      let extra =
+        let diff =
+          Vec3.(
+            mean [ w1.points.bot_right; w1.points.top_right ]
+            <-> mean [ w2.points.bot_left; w2.points.top_left ])
+        in
+        let major =
+          if Float.(abs dx > abs dy) then Vec3.get_x diff else Vec3.get_y diff
+        in
+        Float.(abs (min (abs major -. 5.) 0.))
+      in
+      Vec3.(add (mul d (extra, extra, 0.)))
+    in
+    let outward =
+      Float.(
+        Vec3.(norm @@ (w1.points.top_right <-> w2.points.top_left))
+        > Vec3.(norm @@ (w1.points.top_right <-> w2.points.bot_left)))
+    in
     let starts =
-      [ w1.points.bot_right
+      let up_bot = Wall.Edge.point_at_z w1.edges.bot_right height in
+      [ (if not outward then fudge dir1 w1.points.bot_right else w1.points.bot_right)
       ; w1.points.top_right
       ; Wall.Edge.point_at_z w1.edges.top_right height
-      ; Wall.Edge.point_at_z w1.edges.bot_right height
+      ; (if not outward then fudge dir1 up_bot else up_bot)
       ]
-    in
-    let dests =
-      [ w2.points.bot_left
+      |> List.map ~f:Vec3.(add (mul dir1 (0.1, 0.1, 0.)))
+    and dests =
+      let up_top = Wall.Edge.point_at_z w2.edges.top_left height
+      and up_bot = Wall.Edge.point_at_z w2.edges.bot_left height
+      and slide = fudge (Vec3.negate dir2) in
+      [ ( if outward
+        then slide w2.points.bot_left
+        else
+          w2.points.bot_left
+          (* ; (if not outward then slide w2.points.top_left else w2.points.top_left)
+           * ; (if not outward then slide up_top else up_top) *) )
       ; w2.points.top_left
-      ; Wall.Edge.point_at_z w2.edges.top_left height
-      ; Wall.Edge.point_at_z w2.edges.bot_left height
+      ; up_top
+      ; (if outward then slide up_bot else up_bot)
       ]
+      |> List.map ~f:Vec3.(add (mul dir2 (-0.1, -0.1, 0.)))
     in
     prism_exn starts dests
 end
