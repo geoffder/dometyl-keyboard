bc524fafbd30007b3ea1129149607850ee5e61c2
Geoff deRosenroll <geoffderosenroll@gmail.com>
2021-07-06 23:08:40 -0700

    Fix type t

--- dometyl/lib/columns.ml
+++ dometyl/lib/columns.ml
@@ -1,6 +1,6 @@
 open! Base
 open! Scad_ml
 
-type 'k t = 'k Column.t Map.M(Int).t Map.M(Int).t
+type 'k t = 'k Column.t Map.M(Int).t
 
 let key_exn t col key = Map.find_exn (Map.find_exn t col).Column.keys key
