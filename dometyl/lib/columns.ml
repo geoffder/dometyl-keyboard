db9a8c7d66b36d47c487b5ee43188de83f02b80a
Geoff deRosenroll <geoffderosenroll@gmail.com>
2021-07-18 16:38:56 -0700

    Non-exn version of key.

--- dometyl/lib/columns.ml
+++ dometyl/lib/columns.ml
@@ -3,4 +3,5 @@ open! Scad_ml
 
 type 'k t = 'k Column.t Map.M(Int).t
 
+let key t col key = Option.(Map.find t col >>= fun c -> Map.find c.Column.keys key)
 let key_exn t col key = Map.find_exn (Map.find_exn t col).Column.keys key
