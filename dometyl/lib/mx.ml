d79c13ec2538b71ef7ef90fc5a2c90434caa662e
Geoff deRosenroll <geoffderosenroll@gmail.com>
2021-08-19 22:54:06 -0700

    Begin copying over vals from (derek) cloj

--- dometyl/lib/mx.ml
+++ dometyl/lib/mx.ml
@@ -1,6 +1,13 @@
 open! Base
 open! Scad_ml
 
+let outer_w = 19.
+let inner_w = 13.9
+let inner_h = 13.8
+let thickness = 4.
+let cap_height = 6.25
+let plate_clearance = 3.
+
 module Hotswap = struct
   let ex = Model.import "../things/hotswap.stl" |> Model.color Color.FireBrick
 
@@ -8,16 +15,18 @@ module Hotswap = struct
     Model.import "../things/hotswap-cutout.stl" |> Model.color Color.DarkMagenta
 
   let combo_ex = Model.union [ ex; cutout_ex ]
+
+  (* NOTE: for now, mimic val bindings from cloj, then simplify, and rename *)
+  let mount_width = inner_w +. 3.
+  let mount_height = inner_h +. 3.
+  let holder_x = mount_width
+  let holder_thickness = (holder_x -. inner_w) /. 2.
+  let holder_y = inner_h +. (holder_thickness *. 2.)
+  let swap_z = 3.
+  let square_led_size = 6.
 end
 
-let plate_clearance = 3.
-
 let hole_config =
-  let outer_w = 19.
-  and inner_w = 13.9
-  and inner_h = 13.8
-  and thickness = 4.
-  and cap_height = 6.25 in
   let clip hole =
     let clip =
       Model.rotate
