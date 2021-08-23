77d02e7dcad117e8a54ba638033599295e1b9376
Geoff deRosenroll <geoffderosenroll@gmail.com>
2021-08-23 16:59:40 -0700

    2d difference |> extrusion revamp (+perf?)

--- dometyl/lib/mx.ml
+++ dometyl/lib/mx.ml
@@ -2,21 +2,14 @@ open! Base
 open! Scad_ml
 
 module Hotswap = struct
-  (* let w = inner_w +. 3.
-   * let h = inner_h +. 3. *)
   let holder_thickness = 3.
-  (* let z = holder_thickness /. -2. (\* the bottom of the hole.  *\) *)
+  let socket_z = -2.6 (* hotswap socket cutout position *)
 
-  (* thickness of kailh hotswap holder + some margin of printing error (0.5mm) *)
-  (* let socket_thickness = holder_thickness +. 0.5 *)
-  let socket_z = -2.6
-
-  let make ~inner_w ~inner_h ~thickness facing =
+  let make ~inner_w ~inner_h facing =
     let w = inner_w +. 3.
     and h = inner_h +. 3.
     and z = holder_thickness /. -2. (* the bottom of the hole.  *)
-    (* thickness of kailh hotswap holder + some margin of printing error (0.5mm) *)
-    and socket_thickness = holder_thickness +. 0.5
+    and socket_thickness = holder_thickness +. 0.5 (* plus printing error *)
     and sign =
       match facing with
       | `North -> -1.
@@ -24,46 +17,50 @@ module Hotswap = struct
     in
     let cutout =
       let big =
-        Model.cube ~center:true (w +. 0.01, 4.3, socket_thickness)
-        |> Model.translate (0., 4.95 *. sign, socket_z)
+        Model.square ~center:true (w +. 0.01, 4.3)
+        |> Model.translate (0., 4.95 *. sign, 0.)
       and small =
-        Model.cube ~center:true (w /. 3. *. 1.95, 6.2, socket_thickness)
-        |> Model.translate (w /. -4.5 *. sign, 4. *. sign, socket_z)
+        Model.square ~center:true (w /. 3. *. 1.95, 6.2)
+        |> Model.translate (w /. -4.5 *. sign, 4. *. sign, 0.)
       in
       Model.union [ big; small ]
+      |> Model.linear_extrude ~center:true ~height:socket_thickness
+      |> Model.translate (0., 0., socket_z)
     in
     let hotswap =
       let access_cuts =
         let x = (w /. 2.) -. (w /. 8.04)
         and y = 7.4 in
-        let cut = Model.cube ~center:true (w /. 4., 2.01, socket_thickness) in
+        let cut = Model.square ~center:true (w /. 4., 2.01) in
         Model.union
-          [ Model.translate (x, y *. sign, socket_z) cut
-          ; Model.translate (-.x, y *. sign, socket_z) cut
+          [ Model.translate (x, y *. sign, 0.) cut
+          ; Model.translate (-.x, y *. sign, 0.) cut
           ]
+        |> Model.linear_extrude ~center:true ~height:socket_thickness
+        |> Model.translate (0., 0., socket_z)
       and led_cut =
-        Model.cube ~center:true (6., 6., holder_thickness +. 0.01)
-        |> Model.translate (0., -6. *. sign, z)
+        Model.square ~center:true (6., 6.) |> Model.translate (0., -6. *. sign, 0.)
       and holes =
-        let cyl r = Model.cylinder ~center:true ~fn:30 r (thickness +. 0.01) in
-        let main = cyl 2.05 |> Model.translate (0., 0., z)
-        and pin = cyl 1.65
-        and friction = cyl 0.975 in
-        let plus = Model.translate (-3.81 *. sign, 2.54 *. sign, z) pin
-        and minus = Model.translate (2.54 *. sign, 5.08 *. sign, z) pin
-        and fric_left = Model.translate (-5., 0., z) friction
-        and fric_right = Model.translate (5., 0., z) friction in
+        let main = Model.circle ~fn:30 2.05
+        and pin = Model.circle ~fn:30 1.65
+        and friction = Model.circle ~fn:30 0.975 in
+        let plus = Model.translate (-3.81 *. sign, 2.54 *. sign, 0.) pin
+        and minus = Model.translate (2.54 *. sign, 5.08 *. sign, 0.) pin
+        and fric_left = Model.translate (-5., 0., 0.) friction
+        and fric_right = Model.translate (5., 0., 0.) friction in
         Model.union [ main; plus; minus; fric_left; fric_right ]
       and holder =
-        let slab =
-          Model.cube ~center:true (w, h, holder_thickness) |> Model.translate (0., 0., z)
+        let slab = Model.square ~center:true (w, h)
         and tab =
-          Model.cube ~center:true (w /. 2., 1., holder_thickness)
-          |> Model.translate (0., h /. 2. *. sign, z)
+          Model.square ~center:true (w /. 2., 1.)
+          |> Model.translate (0., h /. 2. *. sign, 0.)
         in
         Model.union [ slab; tab ]
       in
-      Model.difference holder [ access_cuts; led_cut; holes; cutout ]
+      Model.difference holder [ led_cut; holes ]
+      |> Model.linear_extrude ~center:true ~height:holder_thickness
+      |> Model.translate (0., 0., z)
+      |> Fn.flip Model.difference [ access_cuts; cutout ]
     in
     hotswap, cutout
 
@@ -76,7 +73,7 @@ module Hotswap = struct
    *   Model.union [ ex; cutout_ex ] |> Model.translate (0., 0., thickness /. -2.) *)
 
   let combo_ex =
-    let swap, cut = make ~inner_w:13.9 ~inner_h:13.8 ~thickness:4. `North in
+    let swap, cut = make ~inner_w:13.9 ~inner_h:13.8 `North in
     Model.union [ Model.color Color.FireBrick swap; Model.color Color.DarkMagenta cut ]
 end
 
@@ -103,7 +100,7 @@ let make_hole
   let clearance, clip, cutout =
     match hotswap with
     | Some facing ->
-      let swap, cutout = Hotswap.make ~inner_w ~inner_h ~thickness facing in
+      let swap, cutout = Hotswap.make ~inner_w ~inner_h facing in
       let clip hole =
         Model.union
           [ teeth ~inner_h ~thickness hole
