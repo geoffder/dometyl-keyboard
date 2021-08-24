4b5b82b28022eb7dd834a322f084f22228dc3fd2
Geoff deRosenroll <geoffderosenroll@gmail.com>
2021-08-24 12:46:29 -0700

    Add MBK choc keycap

--- dometyl/lib/caps.ml
+++ dometyl/lib/caps.ml
@@ -2,3 +2,4 @@ open! Base
 open! Scad_ml
 
 let sa_r3 = Model.import "../things/SA-R3.stl" |> Model.color Color.DarkSlateBlue
+let mbk = Model.import "../things/MBK_darryldh_1u.stl" |> Model.color Color.DarkSlateBlue
