a4461ec33c3b0b5867684d3806035964dc9b80cf
Geoff deRosenroll <geoffderosenroll@gmail.com>
2021-08-25 22:41:52 -0700

    First attempt to mimic compactyl thumb shape

--- dometyl/lib/boards/deractyl.ml
+++ dometyl/lib/boards/deractyl.ml
@@ -6,28 +6,30 @@ let wall_builder plate =
   Walls.
     { body =
         Body.make
-          ~west_lookup:(fun i -> if i = 0 then Screw else Yes)
-          ~east_lookup:(fun _ -> Yes)
+          ~west_lookup:(fun i -> if i = 1 then Screw else Yes)
+          ~east_lookup:(fun i -> if i = 1 then Screw else Yes)
           ~n_steps:(`PerZ 6.)
           ~clearance:4.
           plate
     ; thumb =
         Thumb.make
           ~east:No
+          ~north_lookup:(fun _ -> No)
           ~south_lookup:(fun i -> if i = 1 then Screw else Yes)
           ~n_steps:(`PerZ 6.)
           plate
     }
 
-let base_connector = Connect.closed ~n_steps:4 ~snake_height:10.
+let base_connector =
+  Connect.closed ~n_steps:4 ~snake_d:2. ~snake_scale:3. ~snake_height:10. ~snake_steps:6
 
 let lookups =
   let offset = function
+    | 0 -> -2.5, -5., 7.5 (* outer index *)
     | 2 -> 0., 3., -5.5 (* middle *)
     | 3 -> 0., 0., 0. (* ring *)
-    | 0 -> -2.5, -5., 7.5 (* outer index *)
-    | 5 -> 5.5, -14., 21. (* outer pinky *)
-    | i when i >= 4 -> 0.5, -12., 9.5 (* pinky *)
+    | 4 -> 0., -12., 9.5 (* pinky *)
+    | 5 -> 5., -14., 21. (* outer pinky *)
     | _ -> 0., -5., 1.
   and curve = function
     | i when i = 0 ->
@@ -63,21 +65,27 @@ let build () =
       ~n_cols:6
       ~spacing:1.
       ~tent:(Float.pi /. 12.)
-      ~thumb_offset:(-16., -49.5, 13.5)
-      ~thumb_angle:Float.(pi /. 12., pi /. -4.75, pi /. 5.5)
+      ~thumb_offset:(7., -52., 11.5)
+      ~thumb_angle:Float.(0., pi /. -3.8, pi /. 6.5)
       ~thumb_curve:
         Curvature.(
           place
-            ~fan:{ angle = Float.pi /. 9.; radius = 70.; tilt = Float.pi /. 24. }
             ~well:{ angle = Float.pi /. 8.; radius = 50.; tilt = 0. }
+            ~fan:{ angle = Float.pi /. 28.; radius = 200.; tilt = 0. }
             ~centre_idx:1)
       ~lookups
       keyhole
   in
   Case.make ~plate_welder ~wall_builder ~base_connector plate
 
-let bastard_compare () =
+let compactyl =
+  Model.import "../things/compactyl_5x6.stl"
+  |> Model.rotate (0., Float.pi /. -8., 0.)
+  |> Model.translate (70., -2., -10.)
+  |> Model.color ~alpha:0.25 Color.DarkSlateBlue
+
+let compactyl_compare () =
   Model.union
-    [ Skeletyl.bastard_skelly
+    [ compactyl
     ; Case.to_scad ~show_caps:false (build ()) |> Model.color ~alpha:0.5 Color.Yellow
     ]
