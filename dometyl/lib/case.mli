685a11e102d7c55ea0effb87940420c17261e7db
Geoff deRosenroll <geoffderosenroll@gmail.com>
2021-08-27 00:39:44 -0700

    Begin the slog of documentation

--- dometyl/lib/case.mli
+++ dometyl/lib/case.mli
@@ -8,10 +8,20 @@ type 'k t =
   ; connections : Connect.t
   }
 
-val translate : Vec3.t -> 'k t -> 'k t
-val rotate : Vec3.t -> 'k t -> 'k t
-val rotate_about_pt : Vec3.t -> Vec3.t -> 'k t -> 'k t
+include Sigs.Transformable' with type 'k t := 'k t
 
+(** [make ~plate_welder ~wall_builder ~base_connector plate]
+
+A high level helper used to contsruct the case from provided functions and a {!Plate.t}.
+- [~plate_welder]: a function intended to generate a {!Model.t} that provides support
+  between columns.
+- [~wall_builder]: will use the provided {!Plate.t} to create {!Walls.t} from the ends and
+  sides of the columns to the ground.
+- [~base_connector]: connects the walls around the perimeter of the case, using various
+  functions provided by {!module:Connect}. This function should return a {!Connect.t} that
+  follows along the entire edge of the case in clockwise fashion without gaps. This way
+  the resulting {!Case.t} will contain the necessary information to use {!module:Bottom}
+  and {!module:Tent}. *)
 val make
   :  plate_welder:('k Plate.t -> Model.t)
   -> wall_builder:('k Plate.t -> Walls.t)
@@ -19,4 +29,9 @@ val make
   -> 'k Plate.t
   -> 'k t
 
+(** [to_scad ?show_caps ?show_cutouts]
+
+Extract the contained {!Model.t}, optionally tacking on keycaps and case cutouts (such as
+representations of hotswap sockets, that are used to provide clearance), stored
+in {!KeyHole.t}. *)
 val to_scad : ?show_caps:bool -> ?show_cutouts:bool -> 'k t -> Model.t
