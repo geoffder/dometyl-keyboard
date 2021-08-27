1995bca2ca7b789b5303e9093115a91c829bcced
Geoff deRosenroll <geoffderosenroll@gmail.com>
2021-08-27 00:38:59 -0700

    Add parameterized type transformable module types

--- dometyl/lib/sigs.ml
+++ dometyl/lib/sigs.ml
@@ -14,3 +14,18 @@ module type Transformable = sig
 
   include Rotatable with type t := t
 end
+
+module type Rotatable' = sig
+  type 'a t
+
+  val rotate : Vec3.t -> 'a t -> 'a t
+  val rotate_about_pt : Vec3.t -> Vec3.t -> 'a t -> 'a t
+end
+
+module type Transformable' = sig
+  type 'a t
+
+  val translate : Vec3.t -> 'a t -> 'a t
+
+  include Rotatable' with type 'a t := 'a t
+end
