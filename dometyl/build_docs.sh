94c628dd16c8b621718a19a68e020f3e0dbb4808
Geoff deRosenroll <geoffderosenroll@gmail.com>
2021-08-28 16:17:01 -0700

    docs up a directory (woops!)

--- dometyl/build_docs.sh
+++ dometyl/build_docs.sh
@@ -3,6 +3,6 @@
 dune build @doc
 if [ -d "_build/default/_doc/_html" ]
 then
-    rm -rf "docs"
-    cp -r "_build/default/_doc/_html" "docs"
+    rm -rf "../docs"
+    cp -r "_build/default/_doc/_html" "../docs"
 fi
