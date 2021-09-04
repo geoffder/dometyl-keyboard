19dc7cad1d1ca1637dd3b1093b41758508ab2f3a
Geoff deRosenroll <geoffderosenroll@gmail.com>
2021-09-03 17:35:06 -0700

    Simplify teeth

--- dometyl/lib/generator/mx.ml
+++ dometyl/lib/generator/mx.ml
@@ -70,11 +70,8 @@ module Hotswap = struct
 end
 
 let teeth ~inner_h ~thickness hole =
-  let block =
-    Model.rotate
-      (Float.pi /. 2., 0., 0.)
-      (Model.cube ~center:true (5., thickness -. 1.3, 0.5))
-  and y = inner_h /. 2. in
+  let block = Model.cube ~center:true (5., 0.51, thickness -. 1.3)
+  and y = (inner_h /. 2.) +. 0.25 in
   let north = Model.translate (0., y, -1.3) block
   and south = Model.translate (0., -.y, -1.3) block in
   Model.difference hole [ north; south ]
