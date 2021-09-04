19f47dd00fdf7e29ccfd756ac6ac00dfad14959a
Geoff deRosenroll <geoffderosenroll@gmail.com>
2021-09-03 17:39:18 -0700

    Add WIP teeth to Choc (need measurments)

--- dometyl/lib/generator/choc.ml
+++ dometyl/lib/generator/choc.ml
@@ -1,6 +1,18 @@
 open! Base
 open! Scad_ml
 
+(* NOTE: y value for block (3.) and its offset (3.6) are eyeballed from datasheet. Need
+   to find a more comprehensive sheet, or measure myself when I get some. *)
+let teeth ~inner_w ~thickness hole =
+  let block = Model.cube ~center:true (0.51, 3., thickness -. 1.3)
+  and x = (inner_w /. 2.) +. 0.25
+  and y = 3.6 in
+  let nw = Model.translate (-.x, y, -1.3) block
+  and sw = Model.translate (-.x, -.y, -1.3) block
+  and ne = Model.translate (x, y, -1.3) block
+  and se = Model.translate (x, -.y, -1.3) block in
+  Model.difference hole [ nw; sw; ne; se ]
+
 let make_hole
     ?cap
     ?(outer_w = 19.)
@@ -21,7 +33,7 @@ let make_hole
       ; inner_w
       ; inner_h
       ; thickness
-      ; clip = Fn.id
+      ; clip = teeth ~inner_w ~thickness
       ; cap_height
       ; clearance
       })
