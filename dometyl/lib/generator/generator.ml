d950bd2963cd499b72c47e41441570c244714744
Geoff deRosenroll <geoffderosenroll@gmail.com>
2021-09-08 01:22:51 -0700

    generator.ml for improved doc generation

---
+++ dometyl/lib/generator/generator.ml
@@ -0,0 +1,27 @@
+open! Base
+open! Scad_ml
+module Case = Case
+module Bottom = Bottom
+module Tent = Tent
+module Ports = Ports
+module BastardShield = BastardShield
+module Connect = Connect
+module Walls = Walls
+module Wall = Wall
+module Screw = Screw
+module Bridge = Bridge
+module Plate = Plate
+module Columns = Columns
+module Column = Column
+module Curvature = Curvature
+module KeyHole = KeyHole
+module Points = Points
+module Bezier = Bezier
+module Mx = Mx
+module Choc = Choc
+module Niz = Niz
+module Caps = Caps
+module Sensor = Sensor
+module Util = Util
+module Infix = Infix
+module Sigs = Sigs
