96fb03123b0ec0b40e2732e9a77b1d7bf9ac1654
Geoff deRosenroll <geoffderosenroll@gmail.com>
2021-09-16 21:43:39 -0700

    Autofmt, -cap (now set in plate ?caps).

--- dometyl/lib/boards/dartyl.ml
+++ dometyl/lib/boards/dartyl.ml
@@ -2,24 +2,24 @@ open! Base
 open! Scad_ml
 open Generator
 
-let bumpon = { Screw.bumpon_config with outer_rad = (2. /. 2. +. 1.); inner_rad = (2. /. 2.)}
+let bumpon =
+  { Screw.bumpon_config with outer_rad = (2. /. 2.) +. 1.; inner_rad = 2. /. 2. }
 
 let wall_builder plate =
   Walls.
-    { body = Body.make 
-        ~n_steps:(`Flat 10) 
-        ~north_clearance: 2.19
-        ~south_clearance: 2.19
-        ~side_clearance: 2.19
-        ~thickness: 5.0
-        ~screw_config:bumpon 
-        plate
+    { body =
+        Body.make
+          ~n_steps:(`Flat 10)
+          ~north_clearance:2.19
+          ~south_clearance:2.19
+          ~side_clearance:2.19
+          ~thickness:5.0
+          ~screw_config:bumpon
+          plate
     ; thumb =
         Thumb.make
           ~north_lookup:(fun _ -> No)
           ~south_lookup:(fun _ -> Yes)
-          (* ~west_lookup:(fun i -> if not (i = 1) then Yes else No) *)
-          (* ~west:No *)
           ~west:Screw
           ~clearance:0.5
           ~d1:3.
@@ -53,12 +53,15 @@ let lookups =
     | 3 -> 0.33, 0., 0. (* ring *)
     | i when i >= 4 -> 0.5, -14., 5. (* pinky *)
     | 0 -> 0., -3., 12. (* inside *)
-    | _ -> -0.66, 0., 5. (* index *)
+    | _ -> -0.66, 0., 5.
+  (* index *)
   and curve = function
     | 0 ->
       Curvature.(
         curve ~well:(spec ~radius:82. (Float.pi /. 12.) ~tilt:(Float.pi /. 10.)) ())
-    | _ -> Curvature.(curve ~well:(spec ~radius:82. (Float.pi /. 12.) ~tilt:(Float.pi /. 15.)) ())
+    | _ ->
+      Curvature.(
+        curve ~well:(spec ~radius:82. (Float.pi /. 12.) ~tilt:(Float.pi /. 15.)) ())
   and swing = function
     | 2 -> Float.pi /. -48.
     | 3 -> Float.pi /. -19.
@@ -72,16 +75,12 @@ let lookups =
   Plate.Lookups.make ~offset ~curve ~splay ~swing ~rows ()
 
 let plate_welder = Plate.skeleton_bridges
-(* let ports_cutter = Ports.make *)
-let ports_cutter = (Ports.reversible_holder 
-  ~reset_button:true 
-  ~x_off:2.7 
-  ~y_off:(-1.9) 
-  ~z_rot:0.09 ())
 
-let build () =
-  (* let keyhole = Mx.make_hole ~cap:Caps.SA.r3 ~clearance:0.5 ~thickness:4.88 ~hotswap:`South () in *)
-  let keyhole = Mx.make_hole ~cap:Caps.SA.r3 ~clearance:0.5 ~thickness:4.88 () in
+let ports_cutter =
+  Ports.reversible_holder ~reset_button:true ~x_off:2.7 ~y_off:(-1.9) ~z_rot:0.09 ()
+
+let build ?hotswap () =
+  let keyhole = Mx.make_hole ~clearance:0.5 ~thickness:4.88 ?hotswap () in
   let plate_builder =
     Plate.make
       ~n_cols:5
@@ -94,5 +93,11 @@ let build () =
       ~thumb_curve:
         Curvature.(curve ~fan:{ angle = Float.pi /. 10.5; radius = 75.; tilt = 0.2 } ())
   in
-  Case.make ~plate_welder ~wall_builder ~base_connector ~ports_cutter ~plate_builder keyhole ~right_hand:false
-
+  Case.make
+    ~plate_welder
+    ~wall_builder
+    ~base_connector
+    ~ports_cutter
+    ~plate_builder
+    keyhole
+    ~right_hand:false
