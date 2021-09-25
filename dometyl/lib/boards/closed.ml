130108275867609fa17835712d92e946ddcf3c51
Geoff deRosenroll <geoffderosenroll@gmail.com>
2021-09-25 15:11:35 -0700

    Update to match modern splaytyl

--- dometyl/lib/boards/closed.ml
+++ dometyl/lib/boards/closed.ml
@@ -12,16 +12,16 @@ let lookups =
     | 2 -> 0., 3.5, -5. (* middle *)
     | 3 -> 1., -2.5, 0.5 (* ring *)
     | i when i >= 4 -> 0.5, -18., 8.5 (* pinky *)
-    | 0 -> -2., 0., 5.5
+    | 0 -> -2.5, 0., 5.
     | _ -> 0., 0., 0.
   and curve = function
     | i when i >= 3 ->
-      Curvature.(curve ~well:(spec ~radius:36. (Float.pi /. 4.25)) ())
+      Curvature.(curve ~well:(spec ~radius:37. (Float.pi /. 4.25)) ())
       (* ring and pinky *)
     | i when i = 0 ->
       Curvature.(
-        curve ~well:(spec ~tilt:(Float.pi /. 7.5) ~radius:45. (Float.pi /. 5.95)) ())
-    | _ -> Curvature.(curve ~well:(spec ~radius:45.5 (Float.pi /. 6.1)) ())
+        curve ~well:(spec ~tilt:(Float.pi /. 7.5) ~radius:46. (Float.pi /. 5.95)) ())
+    | _ -> Curvature.(curve ~well:(spec ~radius:46.5 (Float.pi /. 6.1)) ())
   and splay = function
     | i when i = 3 -> Float.pi /. -25. (* ring *)
     | i when i >= 4 -> Float.pi /. -11. (* pinky *)
@@ -39,8 +39,8 @@ let plate_builder =
           ~fan:{ angle = Float.pi /. 9.; radius = 70.; tilt = Float.pi /. 48. }
           ~well:{ angle = Float.pi /. 7.5; radius = 47.; tilt = 0. }
           ())
-    ~thumb_offset:(-18., -40., 12.)
-    ~thumb_angle:Float.(pi /. 30., pi /. -9., pi /. 12.)
+    ~thumb_offset:(-13., -41., 12.)
+    ~thumb_angle:Float.(pi /. 40., pi /. -14., pi /. 24.)
     ~caps:Caps.Matty3.row
     ~thumb_caps:Caps.MT3.thumb_1u
 
@@ -50,21 +50,30 @@ let wall_builder plate =
         Body.make
           ~west_lookup:(fun i -> if i = 0 then Screw else Yes)
           ~east_lookup:(fun _ -> Yes)
+          ~n_facets:1
           ~n_steps:(`PerZ 6.)
+          ~north_clearance:2.5
+          ~south_clearance:2.5
+          ~side_clearance:1.5
           plate
     ; thumb =
         Thumb.make
+          ~south_lookup:(fun _ -> Yes)
           ~east:No
-          ~south_lookup:(fun i -> if i = 1 then Screw else Yes)
-          ~n_steps:(`PerZ 6.)
+          ~west:Screw
+          ~n_steps:(`Flat 4)
+          ~clearance:3.
           plate
     }
 
 let base_connector =
-  Connect.closed ~n_steps:4 ~west_link:(Connect.straight ~height:15. ())
+  Connect.closed
+    ~n_steps:4
+    ~east_link:(Connect.snake ~height:15. ())
+    ~west_link:(Connect.straight ~height:15. ())
 
 let plate_welder = Plate.column_joins
-let ports_cutter = Ports.make ()
+let ports_cutter = BastardShield.(cutter ~x_off:0. ~y_off:(-1.) (make ()))
 
 let build ?right_hand ?hotswap () =
   let keyhole = Mx.make_hole ?hotswap () in
