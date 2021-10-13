58b7e9a130ff7bde455c085bffb3043c756dc5e4
Geoff deRosenroll <geoffderosenroll@gmail.com>
2021-10-12 23:05:19 -0700

    Add ppx_deriving_scad to setup script

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ cd "$( dirname "${BASH_SOURCE[0]}" )"
 
 # install opam depenencies
 opam --yes install \
-    dune base stdio ppx_jane ppx_inline_test  \
+    dune base stdio ppx_jane ppx_inline_test ppxlib \
     merlin ocp-indent ocaml-lsp-server ocamlformat ocamlformat-rpc
 
 # make user copy of main.ml from example
@@ -28,6 +28,21 @@ fi
 # build and install Scad_ml module in opam switch
 dune build
 opam install ./scad_ml.opam
+
+# clone ppx_deriving_scad into parallel directory
+cd ..
+if [ -d "ppx_deriving_scad" ]
+then
+    cd ppx_deriving_scad
+    git pull
+else
+    git clone https://github.com/geoffder/ppx_deriving_scad
+    cd ppx_deriving_scad
+fi
+
+# build and install the [@@deriving scad] ppx in opam switch
+dune build
+opam install ./ppx_deriving_scad.opam
 
 # return to dometyl and build
 cd ../dometyl-keyboard/dometyl
