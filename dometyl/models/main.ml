f71bf5f51959d0c87b393a66dfeec92ff9b783fe
Geoff deRosenroll <geoffderosenroll@gmail.com>
2022-10-01 18:51:46 -0700

    Experimenting with separate wall generation

--- dometyl/models/main.ml
+++ dometyl/models/main.ml
@@ -68,3 +68,76 @@ let () =
          (*     (fun i -> if i = 0 then Scad.sphere 1. else Scad.sphere 0.1) *)
          (*     k.faces.east.path *)
        ]
+
+let () =
+  let plate = splaytyl_right.plate in
+  let points =
+    let north, south =
+      let f i (col : Column.t) (north, south) =
+        let n = (snd @@ IMap.max_binding col.keys).faces.north.points
+        and s = (snd @@ IMap.min_binding col.keys).faces.south.points in
+        let north = n.top_right :: n.top_left :: north
+        and south = if i > 1 then s.top_left :: s.top_right :: south else south in
+        north, south
+      in
+      let north, south = IMap.fold f plate.body ([], []) in
+      List.rev north, south
+    and west =
+      let f _ (key : Key.t) ps =
+        key.faces.west.points.top_right :: key.faces.west.points.top_left :: ps
+      in
+      List.rev @@ IMap.fold f (snd @@ IMap.min_binding plate.body).keys []
+    and east =
+      let f _ (key : Key.t) ps =
+        key.faces.east.points.top_left :: key.faces.east.points.top_right :: ps
+      in
+      IMap.fold f (snd @@ IMap.max_binding plate.body).keys []
+    and thumb_east, thumb_west =
+      let f _ (col : Column.t) (east, west) =
+        let e = (snd @@ IMap.max_binding col.keys).faces.east.points
+        and w = (snd @@ IMap.min_binding col.keys).faces.west.points in
+        let east = e.top_right :: e.top_left :: east
+        and west = w.top_left :: w.top_right :: west in
+        east, west
+      in
+      let east, west = IMap.fold f plate.thumb ([], []) in
+      List.rev east, west
+    and thumb_north =
+      let f i (key : Key.t) ps =
+        if i < 1
+        then key.faces.north.points.top_right :: key.faces.north.points.top_left :: ps
+        else ps
+      in
+      List.rev @@ IMap.fold f (snd @@ IMap.min_binding plate.thumb).keys []
+    and thumb_south =
+      let f _ (key : Key.t) ps =
+        key.faces.south.points.top_left :: key.faces.south.points.top_right :: ps
+      in
+      IMap.fold f (snd @@ IMap.min_binding plate.thumb).keys []
+    in
+    List.concat
+      [ north; east; south; thumb_east; thumb_south; thumb_west; thumb_north; west ]
+  in
+  let () =
+    Path3.show_points (fun _ -> Scad.(color Color.Red @@ sphere 1.)) points
+    |> Scad.to_file "perimeter_points.scad"
+  in
+  let path =
+    Bezier3.(curve ~fn:256 @@ of_path ~closed:true ~size:(`Flat (`Abs 3.)) points)
+  in
+  let perim_sweep =
+    (* Mesh.path_extrude ~path (Poly2.circle 1.) |> Mesh.to_scad *)
+    Mesh.path_extrude ~euler:true ~path (Poly2.square (v2 1. 1.)) |> Mesh.to_scad
+  in
+  let () = Scad.to_file "perimeter_sweep.scad" perim_sweep in
+  let transforms =
+    Path3.to_transforms ~mode:`Euler (List.map (fun { x; y; z = _ } -> v3 x y 0.) path)
+  in
+  let transforms =
+    let f p t = Affine3.(t %> scale (v3 1. 1. (p.z +. 1.))) in
+    List.map2 f path transforms
+  in
+  Mesh.sweep ~transforms Poly2.(ytrans 0.5 @@ square ~center:true (v2 4. 1.))
+  |> Mesh.to_scad
+  |> Scad.add (Scad.color Color.Red perim_sweep)
+  |> Scad.to_file "perimeter_wall.scad"
